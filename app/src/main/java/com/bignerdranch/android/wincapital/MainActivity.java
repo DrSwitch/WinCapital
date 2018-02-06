c32b791336446c62b0601a5a2ea0824c282347f7
DrSwitch <switchyndra@gmail.com>
2018-02-06 21:22:46 +0800

    разборка с табами

--- app/src/main/java/com/bignerdranch/android/wincapital/MainActivity.java
+++ app/src/main/java/com/bignerdranch/android/wincapital/MainActivity.java
@@ -14,43 +14,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
-        tabLayout.addTab(tabLayout.newTab().setText("Поставленные"));
-        tabLayout.addTab(tabLayout.newTab().setText("Завершенные"));
-
-        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
-
-        final ViewPager viewPager = (ViewPager) findViewById(R.id.pager);
-        final PagerAdapter adapter = new PagerAdapter
-                (getSupportFragmentManager(), tabLayout.getTabCount()) {
-            @Override
-            public int getCount() {
-                return 0;
-            }
-
-            @Override
-            public boolean isViewFromObject(View view, Object object) {
-                return false;
-            }
-        };
-        viewPager.setAdapter(adapter);
-        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
-        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
-            @Override
-            public void onTabSelected(TabLayout.Tab tab) {
-                viewPager.setCurrentItem(tab.getPosition());
-            }
-
-            @Override
-            public void onTabUnselected(TabLayout.Tab tab) {
-
-            }
-
-            @Override
-            public void onTabReselected(TabLayout.Tab tab) {
-
-            }
-        });
     }
 
     public void enterMainButton(View view) {
---
+++ app/src/main/java/com/bignerdranch/android/wincapital/controllers/Reg_Data_Ur_Face.java
@@ -0,0 +1,23 @@
+package com.bignerdranch.android.wincapital.controllers;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.bignerdranch.android.wincapital.R;
+
+/**
+ * Created by DrSwitch on 06.02.2018.
+ */
+
+public class Reg_Data_Ur_Face extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.reg_data_ur_face, container, false);
+    }
+}
