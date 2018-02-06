02ae2b09afa078703220fa22812f2eff76429231
DrSwitch <switchyndra@gmail.com>
2018-02-06 20:44:22 +0800

    first commit

---
+++ app/src/main/java/com/bignerdranch/android/wincapital/MainActivity.java
@@ -0,0 +1,61 @@
+package com.bignerdranch.android.wincapital;
+
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
+        tabLayout.addTab(tabLayout.newTab().setText("Поставленные"));
+        tabLayout.addTab(tabLayout.newTab().setText("Завершенные"));
+
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+
+        final ViewPager viewPager = (ViewPager) findViewById(R.id.pager);
+        final PagerAdapter adapter = new PagerAdapter
+                (getSupportFragmentManager(), tabLayout.getTabCount()) {
+            @Override
+            public int getCount() {
+                return 0;
+            }
+
+            @Override
+            public boolean isViewFromObject(View view, Object object) {
+                return false;
+            }
+        };
+        viewPager.setAdapter(adapter);
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+    }
+
+    public void enterMainButton(View view) {
+
+    }
+
+
+}
---
+++ app/src/main/java/com/bignerdranch/android/wincapital/controllers/RegDataController.java
@@ -0,0 +1,14 @@
+package com.bignerdranch.android.wincapital.controllers;
+
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TabHost;
+
+import com.bignerdranch.android.wincapital.R;
+
+/**
+ * Created by user on 06.02.2018.
+ */
+
+public class RegDataController extends AppCompatActivity {
+
+}
